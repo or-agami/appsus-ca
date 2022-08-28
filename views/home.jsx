327e77fc22095cefea7f02253cf3080d06b0ea33
or-agami <oragami0@gmail.com>
2022-08-28 08:44:02 +0300

    change

--- views/home.jsx
+++ views/home.jsx
@@ -4,7 +4,7 @@ export function Home() {
     return (
         <section className="home">
             <div className="img-container">
-                <img src="../assets/img/home.jpg" />
+                <img src="assets/img/home.jpg" />
                 <h1>Welcome to Appsus!</h1>
                 <div className="work">work.</div>
                 <div className="live">live.</div>
